5e588f15ebb92982b8532be3cd4308b104788993
rijad.dizdarevic <rijad.dizdarevic@gmail.com>
2015-05-25 20:51:38 -0700

    small change to use codeigniter random string helper function instead of writing my own

--- codeigniter/application/models/student_model.php
+++ codeigniter/application/models/student_model.php
@@ -6,6 +6,7 @@ class Student_model extends CI_Model
     public function __construct()
 	{
 			$this->load->database();
+			$this->load->helper('string');
 	}
 
 	//updates all students with random names
@@ -17,7 +18,7 @@ class Student_model extends CI_Model
 		//iterate over the result 
 		Foreach ($query->result() as $row) {
 			//push new random generate names and passwords into array
-			array_push($data, array("id"=>$row->id, "user_name"=>generateRandomString(5), "password"=>generateRandomString(12)) );
+			array_push($data, array("id"=>$row->id, "user_name"=>random_string('alnum', 5), "password"=>random_string('alnum', 12)) );
 		}
 		//update db and return back
 	    return $this->db->update_batch('Student', $data, 'id'); 
@@ -29,17 +30,6 @@ class Student_model extends CI_Model
 		$query = $this->db->get('Student');
 		return $query->result_array();
 	}
-}
-
-//function to generate random string
-function generateRandomString($length = 10) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
 }
 
 ?>
